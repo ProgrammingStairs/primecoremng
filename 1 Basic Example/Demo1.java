554d7b456fd85b4fb125bb0687dbd30928b7e0bb
gauravKothari <kotharigaurav6@gmail.com>
2025-04-12 09:05:24 +0530

    basic program

---
+++ 1 Basic Example/Demo1.java
@@ -0,0 +1,7 @@
+// Program to print Hello World 
+
+class Demo1{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
---
+++ 1 Basic Example/Demo2.java
@@ -0,0 +1,14 @@
+// Program to print Hello World 
+
+class A{}
+class B{
+    public static void main(String args[]){
+        System.out.println("Hello World of class B");
+    }
+}
+class C{}
+class Demo2{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+    }
+}
